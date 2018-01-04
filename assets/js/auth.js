3d7cb2d5f8728ef10dc333a9f449f80f93fd9e37
Vale <valeria.navarrete.h@gmail.com>
2018-01-04 16:16:40 -0300

    subir foto

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -21,7 +21,7 @@ var config = {
 })
 
 $('.pets').click(function(){
-    $('.pets').css("color", "#FA5858");
+    $(this).css("color", "#FA5858");
 });
 
 $('#btn').click(function(){
@@ -32,3 +32,30 @@ $('#btn').click(function(){
 });
 
 $('#modal2').modal();
+
+$('#agree').click(function(){
+ var source = $('#photo-source').val();
+ alert(source);
+ $('#photo-source').val("");
+ var caption = $('#textarea1').val();
+ $('#textarea1').val("");
+ $('#new-post').prepend('<div class="container"><div class="row valign-wrapper"><div class="col s1 m1 l1"><img src="assets/img/logo.png" alt="user-photo" class="user-photo responsive-img circle"></div><div class="col s11 m11 l11"><p><strong>USER-NAME</strong></p></div></div><div class="row"><div class="col s12 m12 l12"><div class="card"><div class="card-image"><img src="" id="photo-new"><a class="btn-floating halfway-fab waves-effect waves-light"><i class="material-icons pets">pets</i></a></div><div class="card-content photo-comment"><p>' 
+  + caption + '</p></div></div></div></div></div><div class="comments"><div class="container"><div id="comment-cont2"></div><ul class="collection"><li class="collection-item avatar"><img src="assets/img/golfo.jpg" alt="" class="circle"><textarea class="txt" id="comment2" placeholder="Añade un comentario..."></textarea><a class="waves-effect waves-teal btn" id="btn2">Submit</a></li></ul></div></div>')
+  $('#photo-new').attr("src", source);
+  $('.pets').click(function(){
+    $(this).css("color", "#FA5858");
+  });
+  $('#btn2').click(function(){
+  var comm = $('#comment2').val();
+  $('#comment2').val("");
+  var contMessage = $('#comment-cont2');
+  contMessage.append('<div class="chip comment-bubble"><img src="assets/img/golfo.jpg" alt="Contact Person">' + comm + '</div>')
+  });
+});
+
+        
+            
+            
+          
+                
+        
